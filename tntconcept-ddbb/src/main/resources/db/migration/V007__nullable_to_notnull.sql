442d25f970e92c6b10e2cf4c5ab6a27e78efb5d4
Ignacio Gómez Oliva <ignacio.gomez.oliva@alumnos.upm.es>
2023-03-07 12:32:29 +0100

    Refactor null on CommissioningPaymentData, CommissioningDelay, Commissioning and CreditTitle

--- tntconcept-ddbb/src/main/resources/db/migration/V007__nullable_to_notnull.sql
+++ tntconcept-ddbb/src/main/resources/db/migration/V007__nullable_to_notnull.sql
@@ -125,3 +125,110 @@ UPDATE `RequestHoliday`
 ALTER TABLE `RequestHoliday`
     MODIFY COLUMN `observations` varchar(1024) NOT NULL,
     MODIFY COLUMN `userComment` varchar(1024) NOT NULL;
+
+-- CreditTitle table
+UPDATE `CreditTitle`
+    SET `concept` = ''
+    WHERE `concept` IS NULL;
+
+UPDATE `CreditTitle`
+    SET `state` = ''
+    WHERE `state` IS NULL;
+
+UPDATE `CreditTitle`
+    SET `type` = ''
+    WHERE `type` IS NULL;
+
+UPDATE `CreditTitle`
+    SET `observations` = ''
+    WHERE `observations` IS NULL;
+
+ALTER TABLE `CreditTitle`
+    MODIFY COLUMN `concept` varchar(1024) NOT NULL,
+    MODIFY COLUMN `state` varchar(16) NOT NULL,
+    MODIFY COLUMN `type` varchar(16) NOT NULL,
+    MODIFY COLUMN `observations` varchar(1024) NOT NULL;
+
+-- Commissioning table
+UPDATE `Commissioning`
+    SET `scope` = ''
+    WHERE `scope` IS NULL;
+
+UPDATE `Commissioning`
+    SET `content` = ''
+    WHERE `content` IS NULL;
+
+UPDATE `Commissioning`
+    SET `products` = ''
+    WHERE `products` IS NULL;
+
+UPDATE `Commissioning`
+    SET `budget` = 0
+    WHERE `budget` IS NULL;
+
+UPDATE `Commissioning`
+    SET `notes` = ''
+    WHERE `notes` IS NULL;
+
+UPDATE `Commissioning`
+    SET `developedActivities` = ''
+    WHERE `developedActivities` IS NULL;
+
+UPDATE `Commissioning`
+    SET `difficultiesAppeared` = ''
+    WHERE `difficultiesAppeared` IS NULL;
+
+UPDATE `Commissioning`
+    SET `results` = ''
+    WHERE `results` IS NULL;
+
+UPDATE `Commissioning`
+    SET `conclusions` = ''
+    WHERE `conclusions` IS NULL;
+
+UPDATE `Commissioning`
+    SET `evaluation` = ''
+    WHERE `evaluation` IS NULL;
+
+UPDATE `Commissioning`
+    SET `status` = ''
+    WHERE `status` IS NULL;
+
+ALTER TABLE `Commissioning`
+    MODIFY COLUMN `scope` varchar(1024) NOT NULL,
+    MODIFY COLUMN `content` varchar(1024) NOT NULL,
+    MODIFY COLUMN `products` varchar(1024) NOT NULL,
+    MODIFY COLUMN `budget` decimal(10, 2) NOT NULL,
+    MODIFY COLUMN `notes` varchar(1024) NOT NULL,
+    MODIFY COLUMN `developedActivities` varchar(1024) NOT NULL,
+    MODIFY COLUMN `difficultiesAppeared` varchar(1024) NOT NULL,
+    MODIFY COLUMN `results` varchar(1024) NOT NULL,
+    MODIFY COLUMN `conclusions` varchar(1024) NOT NULL,
+    MODIFY COLUMN `evaluation` varchar(1024) NOT NULL,
+    MODIFY COLUMN `status` varchar(20) NOT NULL;
+
+-- CommissioningDelay table
+UPDATE `CommissioningDelay`
+    SET `status` = ''
+    WHERE `status` IS NULL;
+
+ALTER TABLE `CommissioningDelay`
+    MODIFY COLUMN `status` varchar(20) NOT NULL;
+
+-- CommissioningPaymentData table
+UPDATE `CommissioningPaymentData`
+    SET `paymentMode` = ''
+    WHERE `paymentMode` IS NULL;
+
+UPDATE `CommissioningPaymentData`
+    SET `bankAccount` = ''
+    WHERE `bankAccount` IS NULL;
+
+UPDATE `CommissioningPaymentData`
+    SET `billNumber` = ''
+    WHERE `billNumber` IS NULL;
+
+ALTER TABLE `CommissioningPaymentData`
+    MODIFY COLUMN `paymentMode` varchar(32) NOT NULL,
+    MODIFY COLUMN `bankAccount` varchar(50) NOT NULL,
+    MODIFY COLUMN `billNumber` varchar(50) NOT NULL;
