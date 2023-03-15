c720b5d6827e425fe455d4f462769e5a5de77ceb
Roberto Pedreira Ulloa <roberto.pedreira@autentia.com>
2023-03-15 16:07:32 +0100

    Unusued entities deleted

--- tntconcept-binnacle/src/main/kotlin/com/autentia/tnt/binnacle/entities/DateInterval.kt
+++ tntconcept-binnacle/src/main/kotlin/com/autentia/tnt/binnacle/entities/DateInterval.kt
@@ -7,7 +7,6 @@ class DateInterval private constructor(val start: LocalDate, val end: LocalDate)
     companion object {
         fun of(start: LocalDate, end: LocalDate) = DateInterval(start, end)
     }
-
-    fun isOneDay() = start.isEqual(end)
+    
     fun includes(localDate: LocalDate) = !localDate.isBefore(start) && !localDate.isAfter(end)
 }
