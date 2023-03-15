9261270065308b9724f28dcbb3871cec27711d0b
Alejandro Pérez García <alejandropg@autentia.com>
2023-03-15 08:19:41 +0100

    refactor: convert class into a data class

--- tntconcept-rest/src/main/kotlin/com/autentia/tnt/api/binnacle/ErrorResponse.kt
+++ tntconcept-rest/src/main/kotlin/com/autentia/tnt/api/binnacle/ErrorResponse.kt
@@ -1,9 +1,6 @@
 package com.autentia.tnt.api.binnacle
 
-import io.swagger.v3.oas.annotations.media.Schema
-
-@Schema(description = "Model for an Error response exception")
-internal class ErrorResponse(
+internal data class ErrorResponse(
     val code: String,
-    val message: String? = "Invalid"
+    val message: String?
 )
