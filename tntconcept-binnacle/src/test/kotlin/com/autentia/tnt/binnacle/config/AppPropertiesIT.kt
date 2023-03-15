8b5c63a0b5e6bfa276e037a9002c0327f9ed13b3
Alejandro Pérez García <alejandropg@autentia.com>
2023-03-15 08:17:15 +0100

    refactor: convert test into a Micronaut integration test

--- tntconcept-binnacle/src/test/kotlin/com/autentia/tnt/binnacle/config/AppPropertiesIT.kt
+++ tntconcept-binnacle/src/test/kotlin/com/autentia/tnt/binnacle/config/AppPropertiesIT.kt
@@ -2,15 +2,19 @@ package com.autentia.tnt.binnacle.config
 
 import com.autentia.tnt.AppProperties
 import io.micronaut.context.ApplicationContext
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import jakarta.inject.Inject
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
-class AppPropertiesTest {
+@MicronautTest
+internal class AppPropertiesIT {
+
+    @Inject
+    private lateinit var context: ApplicationContext
 
     @Test
     fun `recover the first level property version`() {
-        val context = ApplicationContext.run()
-
         val appProperties = context.getBean(AppProperties::class.java)
 
         assertEquals("@project.version@", appProperties.version)
@@ -18,8 +22,6 @@ class AppPropertiesTest {
 
     @Test
     fun `recover a second level property files activity-images`() {
-        val context = ApplicationContext.run()
-
         val appProperties = context.getBean(AppProperties::class.java)
 
         assertEquals("/tmp/activity/images", appProperties.files.activityImages)
@@ -27,8 +29,6 @@ class AppPropertiesTest {
 
     @Test
     fun `recover a 4th level property binnacle work-summary report nameSuffix`() {
-        val context = ApplicationContext.run()
-
         val appProperties = context.getBean(AppProperties::class.java)
 
         assertEquals("summary", appProperties.binnacle.workSummary.report.nameSuffix)
@@ -36,8 +36,6 @@ class AppPropertiesTest {
 
     @Test
     fun `resolve a declarative indirection variable to binnacle vacations_approvers`() {
-        val context = ApplicationContext.run()
-
         val appProperties = context.getBean(AppProperties::class.java)
 
         assertEquals(listOf("approver@example.com", "other@example.com"), appProperties.binnacle.workSummary.mail.to)
