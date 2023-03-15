8f41585ea1c5c331843472eb8a022e494038fa15
Alejandro Pérez García <alejandropg@autentia.com>
2023-03-15 08:25:17 +0100

    feat: use security from archimedes

--- tntconcept-rest/src/main/kotlin/com/autentia/tnt/api/binnacle/UserController.kt
+++ tntconcept-rest/src/main/kotlin/com/autentia/tnt/api/binnacle/UserController.kt
@@ -1,32 +1,34 @@
 package com.autentia.tnt.api.binnacle
 
 import com.autentia.tnt.binnacle.usecases.FindByUserNameUseCase
+import com.autentia.tnt.security.application.SecurityFindQry
 import io.micronaut.http.HttpRequest
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Error
 import io.micronaut.http.annotation.Get
-import io.micronaut.security.annotation.Secured
 import io.swagger.v3.oas.annotations.Operation
-import java.security.Principal
 
 @Controller("/api/user")
 internal class UserController(
-    private val findByUserNameUseCase: FindByUserNameUseCase
+    private val findByUserNameUseCase: FindByUserNameUseCase,
+    private val securityFindQry: SecurityFindQry
 ) {
 
-    @Operation(summary = "Retrieves a logged user")
-    @Get
-    internal fun getLoggedUser(): UserResponse =
+    @Operation(summary = "Retrieves the logged user")
+    @Get("/me")
+    internal fun getUser(): UserResponse =
         UserResponse(findByUserNameUseCase.find())
 
-    @Operation(summary = "Retrieves secured data only for administrator roles")
-    @Get("/secured")
-    @Secured("ROLE_ADMINISTRADOR")
-    internal fun getSecuredData(principal: Principal): String = "Only ADMIN roles can see this text"
+    @Operation(summary = "Retrieves the security info of the logged user")
+    @Get("/me/security")
+    internal fun getUserSecurity(): UserSecurityResponse? {
+        val subject = securityFindQry.find()
+        return if (subject != null) UserSecurityResponse(subject) else null
+    }
 
     @Error
     internal fun onIllegalStateException(request: HttpRequest<*>, e: IllegalStateException) =
-        HttpResponse.unauthorized<ErrorResponse>().body(ErrorResponse("UNAUTHORIZED", e.message))
+        HttpResponse.notFound<Any>()
 
 }
---
+++ tntconcept-rest/src/main/kotlin/com/autentia/tnt/api/binnacle/UserSecurityResponse.kt
@@ -0,0 +1,12 @@
+package com.autentia.tnt.api.binnacle
+
+import io.archimedesfw.security.auth.Subject
+
+internal data class UserSecurityResponse(
+    private val username: String,
+    private val roles: List<String>
+) {
+
+    internal constructor(subject: Subject) : this(subject.principal.name, subject.roles.map { it.role })
+
+}
