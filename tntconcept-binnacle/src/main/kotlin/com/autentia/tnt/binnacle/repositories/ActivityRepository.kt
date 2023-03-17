d4f1aeec483e1e665b2bce273ff986c6e11e5737
Sara Subijana Gracia <sara.subijana@gmail.com>
2023-03-17 13:50:48 +0100

    Fixes query for selecting activities taking into account the start and end interval fields

--- tntconcept-binnacle/src/main/kotlin/com/autentia/tnt/binnacle/repositories/ActivityRepository.kt
+++ tntconcept-binnacle/src/main/kotlin/com/autentia/tnt/binnacle/repositories/ActivityRepository.kt
@@ -15,12 +15,12 @@ internal interface ActivityRepository : CrudRepository<Activity, Long> {
     @EntityGraph(value = "fetch-activity-with-project-and-organization")
     fun findByIdEager(id: Long): Activity?
 
-    @Query("SELECT a FROM Activity a WHERE a.userId= :userId AND a.start BETWEEN :start AND :end")
+    @Query("SELECT a FROM Activity a WHERE a.userId= :userId AND (a.start BETWEEN :start AND :end OR a.end BETWEEN :start AND :end)")
     @EntityGraph(value = "fetch-activity-with-project-and-organization")
     fun getActivitiesBetweenDate(start: LocalDateTime, end: LocalDateTime, userId: Long): List<Activity>
 
     @Query(
-        "SELECT a.start AS start, a.end AS end, a.duration AS duration, a.projectRole.id AS projectRoleId" +
+        "SELECT a.start AS start, a.end AS end, a.duration AS duration, a.projectRole.id AS projectRoleId " +
                 "FROM Activity a WHERE a.userId= :userId AND a.start BETWEEN :start AND :end"
     )
     fun workedMinutesBetweenDate(start: LocalDateTime, end: LocalDateTime, userId: Long): List<ActivityTimeOnly>
