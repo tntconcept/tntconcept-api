bf22e597c26d9624d3e5ea06298eff194617ead9
Sara Subijana Gracia <sara.subijana@gmail.com>
2023-03-23 08:46:44 +0100

    refactor: remove unused endpoint user/me/security

--- tntconcept-rest/src/main/kotlin/com/autentia/tnt/api/binnacle/UserController.kt
+++ tntconcept-rest/src/main/kotlin/com/autentia/tnt/api/binnacle/UserController.kt
@@ -1,7 +1,6 @@
 package com.autentia.tnt.api.binnacle
 
 import com.autentia.tnt.binnacle.usecases.FindByUserNameUseCase
-import com.autentia.tnt.security.application.SecurityFindQry
 import io.micronaut.http.HttpRequest
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Controller
@@ -11,21 +10,13 @@ import io.swagger.v3.oas.annotations.Operation
 
 @Controller("/api/user")
 internal class UserController(
-    private val findByUserNameUseCase: FindByUserNameUseCase,
-    private val securityFindQry: SecurityFindQry
+    private val findByUserNameUseCase: FindByUserNameUseCase
 ) {
 
     @Operation(summary = "Retrieves the logged user")
     @Get("/me")
     internal fun getUser(): UserResponse =
         UserResponse(findByUserNameUseCase.find())
-
-    @Operation(summary = "Retrieves the security info of the logged user")
-    @Get("/me/security")
-    internal fun getUserSecurity(): UserSecurityResponse? {
-        val subject = securityFindQry.find()
-        return if (subject != null) UserSecurityResponse(subject) else null
-    }
 
     @Error
     internal fun onIllegalStateException(request: HttpRequest<*>, e: IllegalStateException) =
