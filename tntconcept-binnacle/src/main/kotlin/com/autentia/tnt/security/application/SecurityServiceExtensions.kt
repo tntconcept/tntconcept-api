668bed984d80d7e57421cf291b448fa8a146ad39
Sara Subijana Gracia <sara.subijana@gmail.com>
2023-04-03 11:26:46 +0200

    feat: handle IllegalStateException in generic rest exception handler

--- tntconcept-binnacle/src/main/kotlin/com/autentia/tnt/security/application/SecurityServiceExtensions.kt
+++ tntconcept-binnacle/src/main/kotlin/com/autentia/tnt/security/application/SecurityServiceExtensions.kt
@@ -2,11 +2,12 @@ package com.autentia.tnt.security.application
 
 import io.micronaut.security.authentication.Authentication
 import io.micronaut.security.utils.SecurityService
+import java.lang.IllegalArgumentException
 
 private const val ADMIN_ROLE = "admin"
 
 fun SecurityService.checkAuthentication(): Authentication =
-    authentication.orElseThrow { IllegalStateException("Required authentication.") }
+    authentication.orElseThrow { IllegalArgumentException("Required authentication.") }
 
 fun SecurityService.checkRole(role: String): Authentication {
     val authentication = checkAuthentication()
