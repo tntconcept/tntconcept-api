4d1ca67e09db63aec017d4ec677900fc061f26e0
Miguel José Rodríguez Martínez <65860772+MiguelJRM95@users.noreply.github.com>
2023-04-21 13:57:30 +0200

    feat: send mail notification to staff when activity requires approval (#19)
    * fix: approval activity notification
    
    * fix: username added in the activity request email
    
    * fix: activityControllerIT tests
    
    * feat: property added for email list of activity approvers
    
    * fix: google smtp host
    
    * feat: tests added to activity creation use case
    
    * feat: change email address by username
    
    * fix: change email address by username in test
    
    ---------
    
    Co-authored-by: MiguelJRM95 <ms-developer-5@mautentia.com>
    Co-authored-by: Sara Subijana Gracia <sara.subijana@gmail.com>
    Co-authored-by: Roberto Pedreira Ulloa <roberto.pedreira@autentia.com>

--- tntconcept-binnacle/src/main/kotlin/com/autentia/tnt/binnacle/usecases/ActivityCreationUseCase.kt
+++ tntconcept-binnacle/src/main/kotlin/com/autentia/tnt/binnacle/usecases/ActivityCreationUseCase.kt
@@ -7,11 +7,13 @@ import com.autentia.tnt.binnacle.entities.dto.ActivityRequestBodyDTO
 import com.autentia.tnt.binnacle.entities.dto.ActivityResponseDTO
 import com.autentia.tnt.binnacle.services.ActivityCalendarService
 import com.autentia.tnt.binnacle.services.ActivityService
+import com.autentia.tnt.binnacle.services.ApproveActivityMailService
 import com.autentia.tnt.binnacle.services.ProjectRoleService
 import com.autentia.tnt.binnacle.services.UserService
 import com.autentia.tnt.binnacle.validators.ActivityValidator
 import io.micronaut.validation.Validated
 import jakarta.inject.Singleton
+import java.util.Locale
 import javax.validation.Valid
 
 @Singleton
@@ -24,10 +26,11 @@ class ActivityCreationUseCase internal constructor(
     private val activityValidator: ActivityValidator,
     private val activityRequestBodyConverter: ActivityRequestBodyConverter,
     private val activityResponseConverter: ActivityResponseConverter,
-    private val timeIntervalConverter: TimeIntervalConverter
+    private val timeIntervalConverter: TimeIntervalConverter,
+    private val approveActivityMailService: ApproveActivityMailService,
 ) {
 
-    fun createActivity(@Valid activityRequestBody: ActivityRequestBodyDTO): ActivityResponseDTO {
+    fun createActivity(@Valid activityRequestBody: ActivityRequestBodyDTO, locale: Locale): ActivityResponseDTO {
         val user = userService.getAuthenticatedUser()
         val projectRole = projectRoleService.getByProjectRoleId(activityRequestBody.projectRoleId)
         val duration = activityCalendarService.getDurationByCountingWorkingDays(
@@ -43,6 +46,10 @@ class ActivityCreationUseCase internal constructor(
                 activityRequest, user
             )
         )
+
+        if (activityResponse.projectRole.isApprovalRequired){
+            approveActivityMailService.sendApprovalActivityMail(activityResponse, user.username, locale)
+        }
 
         return activityResponseConverter.toActivityResponseDTO(activityResponse)
     }
