58a09bf0792fe0635337c54511eb025aea6f09d8
Mario Calín <mariocalinsanchez@gmail.com>
2023-05-11 12:11:51 +0200

    fix: Duration interval in list of activities

--- tntconcept-binnacle/src/main/kotlin/com/autentia/tnt/binnacle/converters/ActivityResponseConverter.kt
+++ tntconcept-binnacle/src/main/kotlin/com/autentia/tnt/binnacle/converters/ActivityResponseConverter.kt
@@ -7,74 +7,55 @@ import jakarta.inject.Singleton
 
 @Singleton
 class ActivityResponseConverter(
-    private val activityIntervalResponseConverter: ActivityIntervalResponseConverter
+        private val activityIntervalResponseConverter: ActivityIntervalResponseConverter
 ) {
 
     fun toActivityResponseDTO(activity: com.autentia.tnt.binnacle.core.domain.Activity) = ActivityResponseDTO(
-        billable = activity.billable,
-        description = activity.description,
-        hasEvidences = activity.hasEvidences,
-        id = activity.id!!,
-        projectRoleId = activity.projectRole.id,
-        interval = activityIntervalResponseConverter.toIntervalResponseDTO(activity),
-        userId = activity.userId,
-        approvalState = activity.approvalState
+            billable = activity.billable,
+            description = activity.description,
+            hasEvidences = activity.hasEvidences,
+            id = activity.id!!,
+            projectRoleId = activity.projectRole.id,
+            interval = activityIntervalResponseConverter.toIntervalResponseDTO(activity),
+            userId = activity.userId,
+            approvalState = activity.approvalState
     )
 
     fun mapActivityToActivityResponseDTO(activity: Activity) = ActivityResponseDTO(
-        billable = activity.billable,
-        description = activity.description,
-        hasEvidences = activity.hasEvidences,
-        id = activity.id!!,
-        projectRoleId = activity.projectRole.id,
-        interval = activityIntervalResponseConverter.mapActivityToIntervalResponseDTO(activity),
-        userId = activity.userId,
-        approvalState = activity.approvalState
+            billable = activity.billable,
+            description = activity.description,
+            hasEvidences = activity.hasEvidences,
+            id = activity.id!!,
+            projectRoleId = activity.projectRole.id,
+            interval = activityIntervalResponseConverter.mapActivityToIntervalResponseDTO(activity),
+            userId = activity.userId,
+            approvalState = activity.approvalState
     )
 
     fun mapActivityToActivityResponse(activity: Activity) = ActivityResponse(
-        id = activity.id!!,
-        start = activity.start,
-        end = activity.end,
-        billable = activity.billable,
-        userId = activity.userId,
-        description = activity.description,
-        organization = activity.projectRole.project.organization,
-        project = activity.projectRole.project,
-        projectRole = activity.projectRole,
-        duration = activity.duration,
-        hasEvidences = activity.hasEvidences,
-        approvalState = activity.approvalState
+            id = activity.id!!,
+            start = activity.start,
+            end = activity.end,
+            billable = activity.billable,
+            userId = activity.userId,
+            description = activity.description,
+            organization = activity.projectRole.project.organization,
+            project = activity.projectRole.project,
+            projectRole = activity.projectRole,
+            duration = activity.duration,
+            hasEvidences = activity.hasEvidences,
+            approvalState = activity.approvalState
     )
 
     fun toActivityResponseDTO(activityResponse: ActivityResponse) =
-        ActivityResponseDTO(
-            activityResponse.billable,
-            activityResponse.description,
-            activityResponse.hasEvidences,
-            activityResponse.id,
-            activityResponse.projectRole.id,
-            activityIntervalResponseConverter.mapActivityResponseToIntervalResponseDTO(activityResponse),
-            activityResponse.userId,
-            activityResponse.approvalState
-        )
-
-    fun mapActivitiesToActivitiesResponseDTO(activities: List<Activity>): List<ActivityResponseDTO> {
-        val activitiesResponseDTO = mutableListOf<ActivityResponseDTO>()
-        activities.forEach { activity ->
-
-            val activityResponseDTO = ActivityResponseDTO(
-                billable = activity.billable,
-                description = activity.description,
-                hasEvidences = activity.hasEvidences,
-                id = activity.id!!,
-                projectRoleId = activity.projectRole.id,
-                interval = activityIntervalResponseConverter.mapActivityToIntervalResponseDTO(activity),
-                userId = activity.userId,
-                approvalState = activity.approvalState
+            ActivityResponseDTO(
+                    activityResponse.billable,
+                    activityResponse.description,
+                    activityResponse.hasEvidences,
+                    activityResponse.id,
+                    activityResponse.projectRole.id,
+                    activityIntervalResponseConverter.mapActivityResponseToIntervalResponseDTO(activityResponse),
+                    activityResponse.userId,
+                    activityResponse.approvalState
             )
-            activitiesResponseDTO.add(activityResponseDTO)
-        }
-        return activitiesResponseDTO
-    }
 }
