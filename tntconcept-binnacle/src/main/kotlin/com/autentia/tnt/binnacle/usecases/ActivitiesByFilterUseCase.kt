126aea7327a7d2562f39bca8529507207c7857ab
Mario <mariocalinsanchez@gmail.com>
2023-05-11 12:29:55 +0200

    fix: Wrong duration in list of activities (#36)
    * fix: Duration interval in list of activities
    
    * fix: Activity aproval duration

--- tntconcept-binnacle/src/main/kotlin/com/autentia/tnt/binnacle/usecases/ActivitiesByFilterUseCase.kt
+++ tntconcept-binnacle/src/main/kotlin/com/autentia/tnt/binnacle/usecases/ActivitiesByFilterUseCase.kt
@@ -19,13 +19,13 @@ import jakarta.inject.Singleton
 
 @Singleton
 class ActivitiesByFilterUseCase internal constructor(
-    private val activityService: ActivityService,
-    private val activityResponseConverter: ActivityResponseConverter,
+        private val activityService: ActivityService,
+        private val activityResponseConverter: ActivityResponseConverter,
 ) {
     fun getActivities(activityFilter: ActivityFilterDTO): List<ActivityResponseDTO> {
         val predicate: Specification<Activity> = getPredicateFromActivityFilter(activityFilter)
         val activities = activityService.getActivities(predicate)
-        return activityResponseConverter.mapActivitiesToActivitiesResponseDTO(activities)
+        return activities.map { activityResponseConverter.toActivityResponseDTO(it) }
     }
 
     private fun getPredicateFromActivityFilter(activityFilter: ActivityFilterDTO): Specification<Activity> {
--- tntconcept-binnacle/src/test/kotlin/com/autentia/tnt/binnacle/usecases/ActivitiesByFilterUseCaseTest.kt
+++ tntconcept-binnacle/src/test/kotlin/com/autentia/tnt/binnacle/usecases/ActivitiesByFilterUseCaseTest.kt
@@ -10,7 +10,7 @@ import com.autentia.tnt.binnacle.entities.dto.ActivityResponseDTO
 import com.autentia.tnt.binnacle.entities.dto.IntervalResponseDTO
 import com.autentia.tnt.binnacle.repositories.predicates.*
 import com.autentia.tnt.binnacle.services.ActivityService
-import org.junit.jupiter.api.Assertions.assertEquals
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
@@ -21,15 +21,15 @@ internal class ActivitiesByFilterUseCaseTest {
     private val activityService = mock<ActivityService>()
     private val activityResponseConverter = mock<ActivityResponseConverter>()
     private val activitiesByFilterUseCase =
-        ActivitiesByFilterUseCase(activityService, activityResponseConverter)
+            ActivitiesByFilterUseCase(activityService, activityResponseConverter)
 
     @Test
     fun `get activities by approval state`() {
         val activityFilterDTO = ActivityFilterDTO(
-            approvalState = ApprovalState.PENDING,
+                approvalState = ApprovalState.PENDING,
         )
         val compositedSpecification =
-            ActivityApprovalStateSpecification(activityFilterDTO.approvalState!!)
+                ActivityApprovalStateSpecification(activityFilterDTO.approvalState!!)
 
         activitiesByFilterUseCase.getActivities(activityFilterDTO)
 
@@ -39,14 +39,14 @@ internal class ActivitiesByFilterUseCaseTest {
     @Test
     fun `get activities by approval state and role id`() {
         val activityFilterDTO = ActivityFilterDTO(
-            approvalState = ApprovalState.PENDING,
-            roleId = 1L
+                approvalState = ApprovalState.PENDING,
+                roleId = 1L
         )
         val compositedSpecification =
-            PredicateBuilder.and(
-                ActivityApprovalStateSpecification(activityFilterDTO.approvalState!!),
-                ActivityRoleIdSpecification(activityFilterDTO.roleId!!)
-            )
+                PredicateBuilder.and(
+                        ActivityApprovalStateSpecification(activityFilterDTO.approvalState!!),
+                        ActivityRoleIdSpecification(activityFilterDTO.roleId!!)
+                )
 
         activitiesByFilterUseCase.getActivities(activityFilterDTO)
 
@@ -56,34 +56,33 @@ internal class ActivitiesByFilterUseCaseTest {
     @Test
     fun `get activities between dates`() {
         val activityFilterDTO = ActivityFilterDTO(
-            startDate = startDate,
-            endDate = endDate,
+                startDate = startDate,
+                endDate = endDate,
         )
         val compositedSpecification =
-            PredicateBuilder.and(
-                ActivityStartDateLessOrEqualSpecification(activityFilterDTO.endDate!!),
-                ActivityEndDateGreaterOrEqualSpecification(activityFilterDTO.startDate!!)
-            )
+                PredicateBuilder.and(
+                        ActivityStartDateLessOrEqualSpecification(activityFilterDTO.endDate!!),
+                        ActivityEndDateGreaterOrEqualSpecification(activityFilterDTO.startDate!!)
+                )
+        
         whenever(activityService.getActivities(compositedSpecification)).thenReturn(listOf(activity))
-        whenever(activityResponseConverter.mapActivitiesToActivitiesResponseDTO(listOf(activity))).thenReturn(
-            activitiesResponseDTO
-        )
+        whenever(activityResponseConverter.toActivityResponseDTO(activity)).thenReturn(activitiesResponseDTO)
 
         val activities = activitiesByFilterUseCase.getActivities(activityFilterDTO)
 
-        val expectedActivities = activitiesResponseDTO
+        val expectedActivity = activitiesResponseDTO
         verify(activityService).getActivities(compositedSpecification)
-        assertEquals(expectedActivities, activities)
+        assertThat(activities).containsExactly(expectedActivity)
     }
 
     @Test
     fun `get activities should ignore project id when role id`() {
         val activityFilterDTO = ActivityFilterDTO(
-            projectId = 1L,
-            roleId = 1L
+                projectId = 1L,
+                roleId = 1L
         )
         val compositedSpecification =
-            ActivityRoleIdSpecification(activityFilterDTO.roleId!!)
+                ActivityRoleIdSpecification(activityFilterDTO.roleId!!)
 
 
         activitiesByFilterUseCase.getActivities(activityFilterDTO)
@@ -94,10 +93,10 @@ internal class ActivitiesByFilterUseCaseTest {
     @Test
     fun `get activities by organization id`() {
         val activityFilterDTO = ActivityFilterDTO(
-            organizationId = 1L,
+                organizationId = 1L,
         )
         val compositedSpecification =
-            ActivityOrganizationIdSpecification(activityFilterDTO.organizationId!!)
+                ActivityOrganizationIdSpecification(activityFilterDTO.organizationId!!)
 
         activitiesByFilterUseCase.getActivities(activityFilterDTO)
 
@@ -107,15 +106,15 @@ internal class ActivitiesByFilterUseCaseTest {
     @Test
     fun `get activities should ignore organization id when project id`() {
         val activityFilterDTO = ActivityFilterDTO(
-            startDate = null,
-            endDate = null,
-            approvalState = null,
-            organizationId = 1L,
-            projectId = 1L,
-            roleId = null
+                startDate = null,
+                endDate = null,
+                approvalState = null,
+                organizationId = 1L,
+                projectId = 1L,
+                roleId = null
         )
         val compositedSpecification =
-            ActivityProjectIdSpecification(activityFilterDTO.projectId!!)
+                ActivityProjectIdSpecification(activityFilterDTO.projectId!!)
 
         activitiesByFilterUseCase.getActivities(activityFilterDTO)
 
@@ -125,20 +124,20 @@ internal class ActivitiesByFilterUseCaseTest {
     @Test
     fun `get activities when multiple parameters`() {
         val activityFilterDTO = ActivityFilterDTO(
-            startDate = startDate,
-            endDate = endDate,
-            approvalState = null,
-            organizationId = 1L,
-            projectId = 1L,
-            roleId = null
+                startDate = startDate,
+                endDate = endDate,
+                approvalState = null,
+                organizationId = 1L,
+                projectId = 1L,
+                roleId = null
         )
         val compositedSpecification =
-            PredicateBuilder.and(
                 PredicateBuilder.and(
-                    ActivityStartDateLessOrEqualSpecification(activityFilterDTO.endDate!!),
-                    ActivityEndDateGreaterOrEqualSpecification(activityFilterDTO.startDate!!)
-                ), ActivityProjectIdSpecification(activityFilterDTO.projectId!!)
-            )
+                        PredicateBuilder.and(
+                                ActivityStartDateLessOrEqualSpecification(activityFilterDTO.endDate!!),
+                                ActivityEndDateGreaterOrEqualSpecification(activityFilterDTO.startDate!!)
+                        ), ActivityProjectIdSpecification(activityFilterDTO.projectId!!)
+                )
 
         activitiesByFilterUseCase.getActivities(activityFilterDTO)
 
@@ -149,41 +148,40 @@ internal class ActivitiesByFilterUseCaseTest {
         val startDate: LocalDate = LocalDate.of(2023, 4, 15)
         val endDate: LocalDate = LocalDate.of(2023, 4, 17)
         val intervalResponseDTO = IntervalResponseDTO(
-            start = startDate.atStartOfDay(),
-            end = endDate.atStartOfDay(),
-            duration = 120,
-            timeUnit = TimeUnit.MINUTES
+                start = startDate.atStartOfDay(),
+                end = endDate.atStartOfDay(),
+                duration = 120,
+                timeUnit = TimeUnit.MINUTES
         )
 
         val projectRole = createProjectRole(1L)
         val activity =
-            Activity(
-                1,
-                startDate.atStartOfDay(),
-                endDate.atStartOfDay(),
-                45,
-                "New activity",
-                projectRole,
-                1,
-                false,
-                null,
-                null,
-                false,
-                approvalState = ApprovalState.PENDING
-            )
+                Activity(
+                        1,
+                        startDate.atStartOfDay(),
+                        endDate.atStartOfDay(),
+                        45,
+                        "New activity",
+                        projectRole,
+                        1,
+                        false,
+                        null,
+                        null,
+                        false,
+                        approvalState = ApprovalState.PENDING
+                ).toDomain()
 
-        val activitiesResponseDTO = listOf(
-            ActivityResponseDTO(
-                billable = false,
-                description = "description",
-                hasEvidences = false,
-                id = 1L,
-                projectRoleId = 1,
-                interval = intervalResponseDTO,
-                userId = 1,
-                approvalState = ApprovalState.PENDING
-            )
-        )
+        val activitiesResponseDTO =
+                ActivityResponseDTO(
+                        billable = false,
+                        description = "description",
+                        hasEvidences = false,
+                        id = 1L,
+                        projectRoleId = 1,
+                        interval = intervalResponseDTO,
+                        userId = 1,
+                        approvalState = ApprovalState.PENDING
+                )
 
     }
 }
