01bbf66997d8c307571e8c16a51f75dd96d4e97f
Mario Calín <mariocalinsanchez@gmail.com>
2023-05-18 16:40:31 +0200

    fix: Delete wrong name folder

--- tntconcept-binnacle/src/test/resources/db.migration/R__test.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO Organization (id, organizationTypeId, organizationISOCategoryId, name, documentNumber, phone,
-                          street,
-                          number, locator, postalCode, city, provinceId, state, countryId, fax, email,
-                          website, ftpsite,
-                          notes, ownerId, departmentId, evaluationCriteria, organizationDocCategoryId,
-                          freelance,
-                          insertDate, updateDate)
-VALUES (3, 1, 1, 'Hermiston, Zieme and Monahan', '', '', '', '', '', '', '', 53, '', 1, '', '', '', '', '', 1, 1, null,
-        1, 0, '2023-04-24 06:33:16', DEFAULT);
