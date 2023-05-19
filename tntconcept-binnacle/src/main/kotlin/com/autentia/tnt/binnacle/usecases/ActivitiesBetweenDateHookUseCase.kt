72f06c66c8188ab35f8d93f7fe681fc1c72825dc
Mario Calín <mariocalinsanchez@gmail.com>
2023-05-19 15:12:30 +0200

    fix: Use case

--- tntconcept-binnacle/src/main/kotlin/com/autentia/tnt/binnacle/usecases/ActivitiesBetweenDateHookUseCase.kt
+++ tntconcept-binnacle/src/main/kotlin/com/autentia/tnt/binnacle/usecases/ActivitiesBetweenDateHookUseCase.kt
@@ -15,7 +15,7 @@ class ActivitiesBetweenDateHookUseCase internal constructor(
 ) {
     fun getActivities(startDate: LocalDate, endDate: LocalDate, userName: String): List<ActivityDateDTO> {
         val user = userService.getByUserName(userName)
-        val activitiesResponse = activityService.getActivitiesBetweenDatesWithoutSecurity(startDate, endDate, user.id)
+        val activitiesResponse = activityService.getUserActivitiesBetweenDates(startDate, endDate, user.id)
         val listActivityDate = activityDateConverter.toListActivityDate(activitiesResponse, startDate, endDate)
         return listActivityDate.map { activityDateConverter.toActivityDateDTO(it) }
     }
