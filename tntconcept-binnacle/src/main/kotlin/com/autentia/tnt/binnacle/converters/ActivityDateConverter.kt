4cf4926875a30454c8cdb11e0a296a606a28eb1e
Mario <mariocalinsanchez@gmail.com>
2023-05-29 10:58:32 +0200

    feat: Remove unnecesary service and put logic in domain (#55)

--- tntconcept-binnacle/src/main/kotlin/com/autentia/tnt/binnacle/converters/ActivityDateConverter.kt
+++ tntconcept-binnacle/src/main/kotlin/com/autentia/tnt/binnacle/converters/ActivityDateConverter.kt
@@ -2,8 +2,6 @@ package com.autentia.tnt.binnacle.converters
 
 import com.autentia.tnt.binnacle.core.domain.ActivitiesResponse
 import com.autentia.tnt.binnacle.core.domain.ActivityDate
-import com.autentia.tnt.binnacle.core.domain.ProjectRoleId
-import com.autentia.tnt.binnacle.core.utils.WorkableProjectRoleIdChecker
 import com.autentia.tnt.binnacle.core.utils.myDatesUntil
 import com.autentia.tnt.binnacle.entities.dto.ActivityDateDTO
 import jakarta.inject.Singleton
@@ -11,17 +9,11 @@ import java.time.LocalDate
 
 @Deprecated("Used in the deprecated Activities Controller")
 @Singleton
-class ActivityDateConverter(
-    private val workableProjectRoleIdChecker: WorkableProjectRoleIdChecker,
-    private val activityResponseConverter: ActivitiesResponseConverter
-) {
+class ActivityDateConverter(private val activityResponseConverter: ActivitiesResponseConverter) {
 
-    fun toActivityDateDTO(activityDate: ActivityDate) =
-        ActivityDateDTO(
-            activityDate.date,
-            activityDate.workedMinutes,
-            activityDate.activities.map { activityResponseConverter.toActivityResponseDTO(it) }
-        )
+    fun toActivityDateDTO(activityDate: ActivityDate) = ActivityDateDTO(activityDate.date,
+        activityDate.workedMinutes,
+        activityDate.activities.map { activityResponseConverter.toActivityResponseDTO(it) })
 
     fun toListActivityDate(
         activities: List<ActivitiesResponse>,
@@ -29,20 +21,16 @@ class ActivityDateConverter(
         endDate: LocalDate,
     ): List<ActivityDate> {
         val activitiesByDate = activities.groupBy { it.startDate.toLocalDate() }
-        val allActivitiesBetweenDates = startDate.myDatesUntil(endDate)
-            .associateWith { emptyList<ActivitiesResponse>() }
-            .toMutableMap()
+        val allActivitiesBetweenDates =
+            startDate.myDatesUntil(endDate).associateWith { emptyList<ActivitiesResponse>() }.toMutableMap()
         allActivitiesBetweenDates.putAll(activitiesByDate)
         return allActivitiesBetweenDates.map(::toActivityDate)
     }
 
     private fun toActivityDate(entry: Map.Entry<LocalDate, List<ActivitiesResponse>>): ActivityDate =
-        ActivityDate(
-            date = entry.key,
-            workedMinutes = entry.value
-                .filter { workableProjectRoleIdChecker.isWorkable(ProjectRoleId(it.projectRole.id)) }
+        ActivityDate(date = entry.key,
+            workedMinutes = entry.value.filter { it.projectRole.isWorkingTime }
                 .fold(0) { acc, activity -> acc + activity.duration },
-            activities = entry.value.sortedBy { it.startDate }
-        )
+            activities = entry.value.sortedBy { it.startDate })
 
 }
--- tntconcept-binnacle/src/main/kotlin/com/autentia/tnt/binnacle/core/utils/WorkableProjectRoleIdChecker.kt
+++
@@ -1,8 +0,0 @@
-package com.autentia.tnt.binnacle.core.utils
-
-import com.autentia.tnt.binnacle.core.domain.ProjectRoleId
-
-class WorkableProjectRoleIdChecker(private val notWorkableRoles: List<ProjectRoleId>) {
-    fun isWorkable(projectRoleId: ProjectRoleId) = !notWorkableRoles.contains(projectRoleId)
-}
-
