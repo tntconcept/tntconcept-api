4750481b2f118bde856053ccddfe7283236dd913
MiguelJRM95 <ms-developer-5@mautentia.com>
2023-06-06 14:23:19 +0200

    refactor: project findbyId method exception handler

--- tntconcept-binnacle/src/main/kotlin/com/autentia/tnt/binnacle/services/ProjectService.kt
+++ tntconcept-binnacle/src/main/kotlin/com/autentia/tnt/binnacle/services/ProjectService.kt
@@ -6,13 +6,20 @@ import com.autentia.tnt.binnacle.repositories.ProjectRepository
 import io.micronaut.transaction.annotation.ReadOnly
 import jakarta.inject.Singleton
 import java.time.LocalDate
-import java.util.*
 import javax.transaction.Transactional
 
 @Singleton
 internal class ProjectService(
     private val projectRepository: ProjectRepository
 ) {
+    @Transactional
+    @ReadOnly
+    fun findById(projectId: Long): Project {
+        val project = projectRepository.findById(projectId).orElseThrow { ProjectNotFoundException(projectId) }
+        return project.toDomain()
+    }
+
+
     fun blockProject(projectId: Long, blockUntil: LocalDate): Project {
         TODO("Not implemented")
     }
@@ -20,11 +27,4 @@ internal class ProjectService(
     fun unblockProject(projectId: Long): Project {
         TODO("Not implemented")
     }
-
-    @Transactional
-    @ReadOnly
-    fun findById(id: Long): Optional<Project> {
-        return projectRepository.findById(id).map { it.toDomain() }
-    }
-
 }
