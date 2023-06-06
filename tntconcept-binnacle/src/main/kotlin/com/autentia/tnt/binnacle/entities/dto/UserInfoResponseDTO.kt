37e4a8a0c87d1e3b26d7193a7feda58aa9cffd25
Sara Subijana Gracia <sara.subijana@gmail.com>
2023-06-06 14:34:17 +0200

    feat: add user id query specification if is requested as query param (#64)

--- tntconcept-binnacle/src/main/kotlin/com/autentia/tnt/binnacle/entities/dto/UserInfoResponseDTO.kt
+++ tntconcept-binnacle/src/main/kotlin/com/autentia/tnt/binnacle/entities/dto/UserInfoResponseDTO.kt
@@ -3,7 +3,8 @@ package com.autentia.tnt.binnacle.entities.dto
 import java.time.LocalDate
 
 data class UserInfoResponseDTO(
+    val id: Long,
     val username: String,
     val hiringDate: LocalDate,
-    val roles: List<String>
+    val roles: List<String>,
 )
