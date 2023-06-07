030e819a65fbb9aaab69bd47fdf89ed54bd2a903
Sara Subijana Gracia <sara.subijana@gmail.com>
2023-06-07 14:15:20 +0200

    refactor: extract validations to private function

--- tntconcept-binnacle/src/main/kotlin/com/autentia/tnt/binnacle/services/ActivityService.kt
+++ tntconcept-binnacle/src/main/kotlin/com/autentia/tnt/binnacle/services/ActivityService.kt
@@ -128,16 +128,20 @@ internal class ActivityService(
     @Transactional(rollbackOn = [Exception::class])
     fun approveActivityById(id: Long): Activity {
         val activityToApprove = activityRepository.findById(id) ?: throw ActivityNotFoundException(id)
-        if (activityToApprove.approvalState == ApprovalState.ACCEPTED || activityToApprove.approvalState == ApprovalState.NA) {
-            throw InvalidActivityApprovalStateException()
-        }
-        if (!activityToApprove.hasEvidences){
-            throw NoEvidenceInActivityException(id)
-        }
+        checkIfActivityCanBeApproved(activityToApprove, id)
         activityToApprove.approvalState = ApprovalState.ACCEPTED
         return activityRepository.update(
             activityToApprove
         )
+    }
+
+    private fun checkIfActivityCanBeApproved(activityToApprove: Activity, id: Long) {
+        if (activityToApprove.approvalState == ApprovalState.ACCEPTED || activityToApprove.approvalState == ApprovalState.NA) {
+            throw InvalidActivityApprovalStateException()
+        }
+        if (!activityToApprove.hasEvidences) {
+            throw NoEvidenceInActivityException(id)
+        }
     }
 
     @Transactional
