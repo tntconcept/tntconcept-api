86da782f4b3390cb840963ba1260ed11be16a732
Sara Subijana Gracia <sara.subijana@gmail.com>
2023-06-09 08:15:09 +0200

    feat: add project-blocker role insert sentence into project block Flyway migration script version

--- tntconcept-ddbb/src/main/resources/db/migration/V008__addProjectBlockFields.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE Project
-    ADD blockDate date;
-
-ALTER TABLE Project
-    ADD blockedByUser int;
-
-ALTER TABLE Project ADD CONSTRAINT `fk_user_id` FOREIGN KEY (`blockedByUser`) REFERENCES `User` (`id`)
