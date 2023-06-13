60d3ba36433275df0d1bc5eb6a6903ba0766f4bc
Francisco Javier Martínez Páez <fjmpaez@autentia.com>
2023-06-13 16:48:53 +0200

    Change project cost date to allocationDate

--- tntconcept-ddbb/src/main/resources/db/migration/V007__projectCostDate.sql
+++ tntconcept-ddbb/src/main/resources/db/migration/V007__projectCostDate.sql
@@ -1,8 +1,5 @@
-ALTER TABLE ProjectCost
-ADD COLUMN date Date;
+ALTER TABLE ProjectCost ADD COLUMN allocationDate DATE;
 
-UPDATE ProjectCost pc
-SET date = (SELECT startDate from Project p where p.id = pc.projectId);
+UPDATE ProjectCost pc SET allocationDate = (SELECT startDate from Project p where p.id = pc.projectId);
 
-ALTER TABLE ProjectCost
-MODIFY date Date NOT NULL;
+ALTER TABLE ProjectCost MODIFY allocationDate DATE NOT NULL;
