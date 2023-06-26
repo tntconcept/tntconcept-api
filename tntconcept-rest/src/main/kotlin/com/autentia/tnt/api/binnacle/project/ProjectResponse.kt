c86dc707504618f797796ac48fefe6d9e4d3b221
Ignacio Rubio <ignacio.rubio@autentia.com>
2023-06-26 14:04:27 +0200

    refactor: add response objects for API project

---
+++ tntconcept-rest/src/main/kotlin/com/autentia/tnt/api/binnacle/project/ProjectResponse.kt
@@ -0,0 +1,32 @@
+package com.autentia.tnt.api.binnacle.project
+
+import com.autentia.tnt.binnacle.entities.dto.ProjectResponseDTO
+import java.time.LocalDate
+
+data class ProjectResponse(
+    val id: Long,
+    val name: String,
+    val open: Boolean,
+    val billable: Boolean,
+    val organizationId: Long,
+    val startDate: LocalDate,
+    val blockDate: LocalDate? = null,
+    val blockedByUser: Long? = null,
+) {
+    companion object {
+        fun from(projectResponseDTO: ProjectResponseDTO) =
+            ProjectResponse(
+                projectResponseDTO.id,
+                projectResponseDTO.name,
+                projectResponseDTO.open,
+                projectResponseDTO.billable,
+                projectResponseDTO.organizationId,
+                projectResponseDTO.startDate,
+                projectResponseDTO.blockDate,
+                projectResponseDTO.blockedByUser,
+            )
+        fun fromNullable(projectResponseDTO: ProjectResponseDTO?) : ProjectResponse? {
+            return if (projectResponseDTO !=null) from(projectResponseDTO) else null
+        }
+    }
+}
