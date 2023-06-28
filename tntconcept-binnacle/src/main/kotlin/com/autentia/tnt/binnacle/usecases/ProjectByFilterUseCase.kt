6564458c2c8aa7a8a7355f2c6084a90e927ff54d
Ignacio Rubio <130980203+irv-autentia@users.noreply.github.com>
2023-06-28 10:00:59 +0200

    refactor: adapt project API to code to stablished conventions (#87)

--- tntconcept-binnacle/src/main/kotlin/com/autentia/tnt/binnacle/usecases/ProjectByFilterUseCase.kt
+++ tntconcept-binnacle/src/main/kotlin/com/autentia/tnt/binnacle/usecases/ProjectByFilterUseCase.kt
@@ -4,22 +4,22 @@ import com.autentia.tnt.binnacle.converters.ProjectResponseConverter
 import com.autentia.tnt.binnacle.entities.Project
 import com.autentia.tnt.binnacle.entities.dto.ProjectFilterDTO
 import com.autentia.tnt.binnacle.entities.dto.ProjectResponseDTO
+import com.autentia.tnt.binnacle.repositories.ProjectRepository
 import com.autentia.tnt.binnacle.repositories.predicates.EmptySpecification
 import com.autentia.tnt.binnacle.repositories.predicates.PredicateBuilder
 import com.autentia.tnt.binnacle.repositories.predicates.ProjectOpenSpecification
 import com.autentia.tnt.binnacle.repositories.predicates.ProjectOrganizationIdSpecification
-import com.autentia.tnt.binnacle.services.ProjectService
 import io.micronaut.data.jpa.repository.criteria.Specification
 import jakarta.inject.Singleton
 
 @Singleton
 class ProjectByFilterUseCase internal constructor(
-    private val projectService: ProjectService,
+    private val projectRepository: ProjectRepository,
     private val projectResponseConverter: ProjectResponseConverter,
 ) {
     fun getProjects(projectFilter: ProjectFilterDTO): List<ProjectResponseDTO> {
         val predicate = getPredicateFromFilter(projectFilter)
-        val projects = projectService.getProjects(predicate)
+        val projects = projectRepository.findAll(predicate).map { it.toDomain() }
         return projects.map {
             projectResponseConverter.toProjectResponseDTO(it)
         }
