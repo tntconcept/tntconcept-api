f949941885169383f77647c454403c5c7799d4c4
Ignacio Rubio <130980203+irv-autentia@users.noreply.github.com>
2023-06-28 12:09:46 +0200

    fix: add transactional in order to fix the error at retrieve data (#89)

--- tntconcept-binnacle/src/main/kotlin/com/autentia/tnt/binnacle/usecases/ProjectByFilterUseCase.kt
+++ tntconcept-binnacle/src/main/kotlin/com/autentia/tnt/binnacle/usecases/ProjectByFilterUseCase.kt
@@ -10,13 +10,17 @@ import com.autentia.tnt.binnacle.repositories.predicates.PredicateBuilder
 import com.autentia.tnt.binnacle.repositories.predicates.ProjectOpenSpecification
 import com.autentia.tnt.binnacle.repositories.predicates.ProjectOrganizationIdSpecification
 import io.micronaut.data.jpa.repository.criteria.Specification
+import io.micronaut.transaction.annotation.ReadOnly
 import jakarta.inject.Singleton
+import javax.transaction.Transactional
 
 @Singleton
 class ProjectByFilterUseCase internal constructor(
     private val projectRepository: ProjectRepository,
     private val projectResponseConverter: ProjectResponseConverter,
 ) {
+    @Transactional
+    @ReadOnly
     fun getProjects(projectFilter: ProjectFilterDTO): List<ProjectResponseDTO> {
         val predicate = getPredicateFromFilter(projectFilter)
         val projects = projectRepository.findAll(predicate).map { it.toDomain() }
