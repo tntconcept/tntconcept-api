8d2eb2f3db0a9e5ae676f8ac0042f6dc9754eafe
David Villa Jimenez <david.villa@autentia.com>
2023-07-14 17:28:24 +0200

    refactor: add 'exception' to name

--- tntconcept-binnacle/src/main/kotlin/com/autentia/tnt/binnacle/exception/ActivityBeforeProjectCreationDateException.kt
+++ tntconcept-binnacle/src/main/kotlin/com/autentia/tnt/binnacle/exception/ActivityBeforeProjectCreationDateException.kt
@@ -1,5 +1,5 @@
 package com.autentia.tnt.binnacle.exception
 
-class ActivityBeforeProjectCreationDate(message: String) : BinnacleException(message) {
+class ActivityBeforeProjectCreationDateException(message: String) : BinnacleException(message) {
     constructor() : this("The start of the activity is prior to the date the project was created.")
 }
