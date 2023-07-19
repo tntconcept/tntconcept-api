5be9f3c462f34dbdee122d128f8b65cafd9d81ca
cgmarcos <cgmarcos@autentia.com>
2023-07-19 09:53:18 +0200

    Refactor Error with timeLimits to return timelimits in corresponding units.

--- tntconcept-binnacle/src/main/kotlin/com/autentia/tnt/binnacle/exception/MaxTimePerActivityRoleException.kt
+++ tntconcept-binnacle/src/main/kotlin/com/autentia/tnt/binnacle/exception/MaxTimePerActivityRoleException.kt
@@ -4,7 +4,9 @@ import com.autentia.tnt.binnacle.entities.TimeUnit
 
 class MaxTimePerActivityRoleException(
     val maxAllowedTime: Int,
-    val timeUnit: TimeUnit
+    val remainingTime: Int,
+    val timeUnit: TimeUnit,
+    val year: Int,
 ) : BinnacleException(
     "Reached max registrable duration limit by activity. Max allowed time: $maxAllowedTime $timeUnit"
 )
