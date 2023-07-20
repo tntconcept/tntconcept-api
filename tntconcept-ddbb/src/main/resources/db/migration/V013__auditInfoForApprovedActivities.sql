4a50d94fefbab917d3711885c3a2107e91cbbcee
David Isla <david.isla@autentia.com>
2023-07-20 08:42:25 +0200

    feat: Add column init value for previous accepted activities

--- tntconcept-ddbb/src/main/resources/db/migration/V013__auditInfoForApprovedActivities.sql
+++ tntconcept-ddbb/src/main/resources/db/migration/V013__auditInfoForApprovedActivities.sql
@@ -5,3 +5,5 @@ ALTER TABLE Activity
     ADD approvalDate date;
 
 ALTER TABLE Activity ADD CONSTRAINT `fk_activity_approvedUserId` FOREIGN KEY (`approvedByUserId`) REFERENCES `User` (`id`);
+
+UPDATE Activity SET approvalDate = DATE(updateDate) WHERE approvalState = 'ACCEPTED';
