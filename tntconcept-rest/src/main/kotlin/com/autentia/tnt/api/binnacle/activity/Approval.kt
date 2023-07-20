946060708e7ffd940fac9d86265a2be866ef6dc4
David Isla <david.isla@autentia.com>
2023-07-20 09:20:51 +0200

    feat: Dont send null values on approval structure

--- tntconcept-rest/src/main/kotlin/com/autentia/tnt/api/binnacle/activity/Approval.kt
+++ tntconcept-rest/src/main/kotlin/com/autentia/tnt/api/binnacle/activity/Approval.kt
@@ -1,8 +1,10 @@
 package com.autentia.tnt.api.binnacle.activity
 
 import com.autentia.tnt.binnacle.entities.ApprovalState
+import com.fasterxml.jackson.annotation.JsonInclude
 import java.time.LocalDateTime
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 data class Approval(
         val state: ApprovalState,
         val approvedByUserId: Long? = null,
