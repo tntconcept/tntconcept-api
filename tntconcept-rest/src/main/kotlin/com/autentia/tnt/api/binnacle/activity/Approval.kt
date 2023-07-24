b7c18a82c59c235ad95efcc0ad543c3eb4ed7f56
Mario Calín <mariocalinsanchez@gmail.com>
2023-07-24 13:43:53 +0200

    feat: Add flag to rest response

--- tntconcept-rest/src/main/kotlin/com/autentia/tnt/api/binnacle/activity/Approval.kt
+++ tntconcept-rest/src/main/kotlin/com/autentia/tnt/api/binnacle/activity/Approval.kt
@@ -7,6 +7,7 @@ import java.time.LocalDateTime
 @JsonInclude(JsonInclude.Include.NON_NULL)
 data class Approval(
         val state: ApprovalState,
+        val canBeApproved: Boolean = false,
         val approvedByUserId: Long? = null,
         val approvalDate: LocalDateTime? = null
 )
