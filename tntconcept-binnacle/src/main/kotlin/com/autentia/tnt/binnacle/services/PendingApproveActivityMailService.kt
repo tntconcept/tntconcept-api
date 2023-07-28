eacdeb37be23fc064c99098796982e3dae041724
Mario Calín <mariocalinsanchez@gmail.com>
2023-07-28 11:01:45 +0200

    feat: Merge email destinations

--- tntconcept-binnacle/src/main/kotlin/com/autentia/tnt/binnacle/services/PendingApproveActivityMailService.kt
+++ tntconcept-binnacle/src/main/kotlin/com/autentia/tnt/binnacle/services/PendingApproveActivityMailService.kt
@@ -12,9 +12,9 @@ import java.util.stream.Stream
 
 @Singleton
 internal class PendingApproveActivityMailService(
-    private val mailService: MailService,
-    private val messageSource: MessageSource,
-    private val appProperties: AppProperties
+        private val mailService: MailService,
+        private val messageSource: MessageSource,
+        private val appProperties: AppProperties
 ) {
     fun sendApprovalActivityMail(activity: Activity, username: String, locale: Locale,
                                  listOfActivityApprovalUserEmails: List<String>) {
@@ -23,28 +23,31 @@ internal class PendingApproveActivityMailService(
             return
         }
         val body = messageSource
-            .getMessage(
-                "mail.request.pendingApproveActivity.template",
-                locale,
-                username,
-                activity.getStart().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")),
-                activity.getEnd().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")),
-                activity.description
-            )
-            .orElse(null) ?: error("Cannot find message mail.request.pendingApproveActivity.template")
+                .getMessage(
+                        "mail.request.pendingApproveActivity.template",
+                        locale,
+                        username,
+                        activity.getStart().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")),
+                        activity.getEnd().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")),
+                        activity.description
+                )
+                .orElse(null) ?: error("Cannot find message mail.request.pendingApproveActivity.template")
 
         val subject = messageSource
-            .getMessage("mail.request.pendingApproveActivity.subject", locale, username)
-            .orElse(null) ?: error("Cannot find message mail.request.pendingApproveActivity.subject")
+                .getMessage("mail.request.pendingApproveActivity.subject", locale, username)
+                .orElse(null) ?: error("Cannot find message mail.request.pendingApproveActivity.subject")
 
 
-        val emailDestinations = Stream.concat(
-                listOfActivityApprovalUserEmails.stream(),
-                appProperties.binnacle.activitiesApprovers.stream())
-                .collect(Collectors.toSet())
+        val emailDestinations = mergeAndRemoveDuplicates(appProperties.binnacle.activitiesApprovers,
+                listOfActivityApprovalUserEmails)
 
         mailService.send(appProperties.mail.from, emailDestinations.toList(), subject, body)
-            .onFailure { logger.error("Error sending activity approve email", it) }
+                .onFailure { logger.error("Error sending activity approve email", it) }
+    }
+
+    fun mergeAndRemoveDuplicates(list1: List<String>, list2: List<String>): List<String> {
+        val mergedList = list1 + list2
+        return mergedList.distinct()
     }
 
     private companion object {
---
+++ tntconcept-binnacle/src/test/kotlin/com/autentia/tnt/binnacle/services/PendingApproveActivityMailServiceTest.kt
@@ -0,0 +1,48 @@
+package com.autentia.tnt.binnacle.services
+
+import com.autentia.tnt.AppProperties
+import com.autentia.tnt.binnacle.config.createActivity
+import io.micronaut.context.MessageSource
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.mockito.Mockito.mock
+import org.mockito.kotlin.any
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+import java.time.format.DateTimeFormatter
+import java.util.*
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class PendingApproveActivityMailServiceTest {
+
+    private val mailService = mock<MailService>()
+    private val messageSource = mock<MessageSource>()
+    private val appProperties = AppProperties()
+
+    private val sut = PendingApproveActivityMailService(mailService, messageSource, appProperties)
+
+    @Test
+    fun `should send email to approve users from app properties and from method parameter`() {
+        // Given
+        appProperties.mail.enabled = true
+        appProperties.mail.from = "fromTest@email.com"
+        appProperties.binnacle.activitiesApprovers = listOf("approver@email.com")
+
+        val anActivity = createActivity().toDomain()
+        val username = "myuser"
+        val aLocale = Locale.ENGLISH
+        val listOfApprovalUsers = listOf("approver@email.com", "two@email.com", "three@email.com")
+
+        whenever(messageSource.getMessage("mail.request.pendingApproveActivity.template", aLocale, username, anActivity.getStart().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")), anActivity.getEnd().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")), anActivity.description)).thenReturn(Optional.of("Message"))
+        whenever(messageSource.getMessage("mail.request.pendingApproveActivity.subject", aLocale, username)).thenReturn(Optional.of("Subject"))
+
+        whenever(this.mailService.send(any(), any(), any(), any(), any())).thenReturn(Result.success("Email sent"))
+
+        // When
+        sut.sendApprovalActivityMail(anActivity, username, aLocale, listOfApprovalUsers)
+
+        // Then
+        val expectedDestination = listOf("approver@email.com", "two@email.com", "three@email.com")
+        verify(this.mailService).send(appProperties.mail.from, expectedDestination, "Subject", "Message")
+    }
+}
