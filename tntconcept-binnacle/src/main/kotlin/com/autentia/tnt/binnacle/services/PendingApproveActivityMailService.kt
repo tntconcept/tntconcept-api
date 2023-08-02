dfd03edd3bf09a2ff7f985495f02b1c5ca5cac57
antonio-rufino <antonio.rufino@autentia.com>
2023-08-02 10:25:30 +0200

    feat: send time with activities in minutes

--- tntconcept-binnacle/src/main/kotlin/com/autentia/tnt/binnacle/services/PendingApproveActivityMailService.kt
+++ tntconcept-binnacle/src/main/kotlin/com/autentia/tnt/binnacle/services/PendingApproveActivityMailService.kt
@@ -2,48 +2,59 @@ package com.autentia.tnt.binnacle.services
 
 import com.autentia.tnt.AppProperties
 import com.autentia.tnt.binnacle.core.domain.Activity
+import com.autentia.tnt.binnacle.entities.TimeUnit
 import io.micronaut.context.MessageSource
 import jakarta.inject.Singleton
 import org.slf4j.LoggerFactory
 import java.time.format.DateTimeFormatter
-import java.util.Locale
-import java.util.stream.Collectors
-import java.util.stream.Stream
+import java.util.*
 
 @Singleton
 internal class PendingApproveActivityMailService(
-        private val mailService: MailService,
-        private val messageSource: MessageSource,
-        private val appProperties: AppProperties
+    private val mailService: MailService,
+    private val messageSource: MessageSource,
+    private val appProperties: AppProperties,
 ) {
-    fun sendApprovalActivityMail(activity: Activity, username: String, locale: Locale,
-                                 listOfActivityApprovalUserEmails: List<String>) {
+    fun sendApprovalActivityMail(
+        activity: Activity, username: String, locale: Locale,
+        listOfActivityApprovalUserEmails: List<String>,
+    ) {
+
+        val datePattern = getPatternOfActivityTimeUnit(activity)
+
         if (!appProperties.mail.enabled) {
             logger.info("Mailing of approval activities is disabled")
             return
         }
+        
         val body = messageSource
-                .getMessage(
-                        "mail.request.pendingApproveActivity.template",
-                        locale,
-                        username,
-                        activity.getStart().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")),
-                        activity.getEnd().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")),
-                        activity.description
-                )
-                .orElse(null) ?: error("Cannot find message mail.request.pendingApproveActivity.template")
+            .getMessage(
+                "mail.request.pendingApproveActivity.template",
+                locale,
+                username,
+                activity.getStart().format(DateTimeFormatter.ofPattern(datePattern)),
+                activity.getEnd().format(DateTimeFormatter.ofPattern(datePattern)),
+                activity.description
+            )
+            .orElse(null) ?: error("Cannot find message mail.request.pendingApproveActivity.template")
 
         val subject = messageSource
-                .getMessage("mail.request.pendingApproveActivity.subject", locale, username)
-                .orElse(null) ?: error("Cannot find message mail.request.pendingApproveActivity.subject")
+            .getMessage("mail.request.pendingApproveActivity.subject", locale, username)
+            .orElse(null) ?: error("Cannot find message mail.request.pendingApproveActivity.subject")
 
 
-        val emailDestinations = mergeAndRemoveDuplicates(appProperties.binnacle.activitiesApprovers,
-                listOfActivityApprovalUserEmails)
+        val emailDestinations = mergeAndRemoveDuplicates(
+            appProperties.binnacle.activitiesApprovers,
+            listOfActivityApprovalUserEmails
+        )
 
         mailService.send(appProperties.mail.from, emailDestinations.toList(), subject, body)
-                .onFailure { logger.error("Error sending activity approve email", it) }
+            .onFailure { logger.error("Error sending activity approve email", it) }
     }
+
+    fun getPatternOfActivityTimeUnit(activity: Activity) =
+        if (activity.timeUnit == TimeUnit.MINUTES) "yyyy-MM-dd HH:mm:ss" else "yyyy-MM-dd"
+
 
     fun mergeAndRemoveDuplicates(list1: List<String>, list2: List<String>): List<String> {
         val mergedList = list1 + list2
