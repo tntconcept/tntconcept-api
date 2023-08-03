535e43a80c9d1602dafa39311f0f0c405bb0bd27
cgmarcos <cgmarcos@autentia.com>
2023-08-03 17:39:59 +0200

    Configure micronaut properties to enable multipart and to limit multipart max-file-size to 100MModify /activity/{activityId}/evidence Controller to return Multipart.

--- tntconcept-rest/src/main/kotlin/com/autentia/tnt/api/binnacle/activity/evidence/EvidenceController.kt
+++ tntconcept-rest/src/main/kotlin/com/autentia/tnt/api/binnacle/activity/evidence/EvidenceController.kt
@@ -6,12 +6,15 @@ import com.autentia.tnt.binnacle.exception.NoEvidenceInActivityException
 import com.autentia.tnt.binnacle.usecases.ActivityEvidenceRetrievalUseCase
 import io.micronaut.http.HttpRequest
 import io.micronaut.http.HttpResponse
+import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Error
 import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.Produces
 import io.micronaut.validation.Validated
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
+import java.util.*
 
 @Controller("/api/activity/{activityId}/evidence")
 @Validated
@@ -21,9 +24,17 @@ internal class EvidenceController (
 ){
 
     @Get
+    @Produces(MediaType.MULTIPART_FORM_DATA)
     @Operation(summary = "Retrieves an activity evidence by the activity id.")
-    internal fun getEvidenceByActivityId(activityId: Long): String =
-        activityEvidenceRetrievalUseCase.getActivityEvidenceByActivityId(activityId).getDataUrl()
+    internal fun getEvidenceByActivityId(activityId: Long): HttpResponse<ByteArray> {
+        val evidence = activityEvidenceRetrievalUseCase.getActivityEvidenceByActivityId(activityId)
+
+        return HttpResponse.ok(Base64.getDecoder().decode(evidence.base64data))
+            .header("Content-type",  evidence.mediaType)
+            .header("Content-disposition", "attachment; filename=\"${activityId}_evidence.${getExtension(evidence.mediaType)}\"");
+    }
+
+    fun getExtension(mediaType: String) = mediaType.split("/")[1]
 
 
     @Error
--- tntconcept-rest/src/test/kotlin/com/autentia/tnt/api/binnacle/activity/evidence/EvidenceControllerIT.kt
+++ tntconcept-rest/src/test/kotlin/com/autentia/tnt/api/binnacle/activity/evidence/EvidenceControllerIT.kt
@@ -11,13 +11,14 @@ import io.micronaut.http.client.annotation.Client
 import io.micronaut.test.annotation.MockBean
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import jakarta.inject.Inject
-import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.whenever
+import java.util.*
 
 @MicronautTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -39,21 +40,27 @@ class EvidenceControllerIT {
 
     @Test
     fun `get an evidence activity by id`() {
+        val evidence = EvidenceDTO.from(EVIDENCE_BASE64)
 
-        doReturn(EvidenceDTO.from(ACTIVITY_IMAGE)).whenever(activityEvidenceRetrievalUseCase)
+        doReturn(evidence).whenever(activityEvidenceRetrievalUseCase)
             .getActivityEvidenceByActivityId(ACTIVITY_ID)
 
         val response = client.exchangeObject<String>(
             HttpRequest.GET("/api/activity/$ACTIVITY_ID/evidence")
         )
 
-        Assertions.assertEquals(HttpStatus.OK, response.status)
-        Assertions.assertEquals(ACTIVITY_IMAGE, response.body())
+        assertEquals(HttpStatus.OK, response.status)
+        assertEquals(EVIDENCE_MIME_TYPE, response.headers.get("Content-type"))
+        assertEquals( String(IMAGE_RAW), response.body())
+        assertEquals("attachment; filename=\"${EVIDENCE_FILENAME}\"", response.headers.get("Content-disposition"))
     }
 
     private companion object {
         private const val ACTIVITY_ID = 2L
-        private const val ACTIVITY_IMAGE =
-            "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVQYV2NgYAAAAAMAAWgmWQ0AAAAASUVORK5CYII="
+        private val IMAGE_BASE64 = "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVQYV2NgYAAAAAMAAWgmWQ0AAAAASUVORK5CYII="
+        private val IMAGE_RAW = Base64.getDecoder().decode(IMAGE_BASE64)
+        private val EVIDENCE_FILENAME = "${ACTIVITY_ID}_evidence.png"
+        private const val EVIDENCE_MIME_TYPE = "image/png"
+        private val EVIDENCE_BASE64 = "data:$EVIDENCE_MIME_TYPE;base64,$IMAGE_BASE64"
     }
 }
