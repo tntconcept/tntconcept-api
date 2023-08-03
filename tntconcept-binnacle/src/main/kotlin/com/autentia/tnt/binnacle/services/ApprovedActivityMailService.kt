dbe25c4a6f03d9fa3c9c603607a267ddc93ab0e0
antonio-rufino <antonio.rufino@autentia.com>
2023-08-03 12:31:54 +0200

    feat: notify the petitioner according to time unit

--- tntconcept-binnacle/src/main/kotlin/com/autentia/tnt/binnacle/services/ApprovedActivityMailService.kt
+++ tntconcept-binnacle/src/main/kotlin/com/autentia/tnt/binnacle/services/ApprovedActivityMailService.kt
@@ -2,6 +2,7 @@ package com.autentia.tnt.binnacle.services
 
 import com.autentia.tnt.AppProperties
 import com.autentia.tnt.binnacle.core.domain.Activity
+import com.autentia.tnt.binnacle.entities.TimeUnit
 import com.autentia.tnt.binnacle.entities.User
 import io.micronaut.context.MessageSource
 import jakarta.inject.Singleton
@@ -13,19 +14,22 @@ import java.util.*
 internal class ApprovedActivityMailService(
     private val mailService: MailService,
     private val messageSource: MessageSource,
-    private val appProperties: AppProperties
+    private val appProperties: AppProperties,
 ) {
-    fun sendApprovedActivityMail(activity: Activity, user: User, locale: Locale){
+    fun sendApprovedActivityMail(activity: Activity, user: User, locale: Locale) {
         if (!appProperties.mail.enabled) {
             logger.info("Mailing of approval activities is disabled")
             return
         }
+
+        val datePattern = getPatternOfActivityByTimeUnit(activity)
+
         val body = messageSource
             .getMessage(
                 "mail.request.approvedActivity.template",
                 locale,
-                activity.timeInterval.start.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")),
-                activity.timeInterval.end.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")),
+                activity.timeInterval.start.format(DateTimeFormatter.ofPattern(datePattern)),
+                activity.timeInterval.end.format(DateTimeFormatter.ofPattern(datePattern)),
                 activity.description
             )
             .orElse(null) ?: error("Cannot find message mail.request.approvedActivity.template")
@@ -37,6 +41,9 @@ internal class ApprovedActivityMailService(
         mailService.send(appProperties.mail.from, listOf(user.email), subject, body)
             .onFailure { logger.error("Error sending activity approve email", it) }
     }
+
+    fun getPatternOfActivityByTimeUnit(activity: Activity) =
+        if (activity.timeUnit == TimeUnit.MINUTES) "yyyy-MM-dd HH:mm:ss" else "yyyy-MM-dd"
 
     private companion object {
         private val logger = LoggerFactory.getLogger(ApprovedActivityMailService::class.java)
