d4d79a4e39f3597ffd66bc7a2274f353617c94fe
antonio-rufino <antonio.rufino@autentia.com>
2023-08-14 12:31:33 +0200

    refactor: create or replace table Attachment

--- tntconcept-ddbb/src/main/resources/db/migration/V015__attachments.sql
+++ tntconcept-ddbb/src/main/resources/db/migration/V015__attachments.sql
@@ -1,4 +1,6 @@
-CREATE TABLE Attachment (
+DROP TABLE IF EXISTS `Attachment`;
+
+CREATE TABLE `Attachment` (
     id              VARCHAR(36)  NOT NULL,
     userId          INT          NOT NULL,
     path            VARCHAR(255) NOT NULL,
