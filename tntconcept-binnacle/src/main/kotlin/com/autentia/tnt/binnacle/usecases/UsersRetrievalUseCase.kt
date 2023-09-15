db79e197246f60617b65a7ab3619dabfb1177e1f
Gregorio Bujor Santos <goyobujor@gmail.com>
2023-09-15 10:41:17 +0200

    feat: change getAllActiveUsers to getUsers & add predicates into UsersRetrievalUseCase

--- tntconcept-binnacle/src/main/kotlin/com/autentia/tnt/binnacle/usecases/UsersRetrievalUseCase.kt
+++ tntconcept-binnacle/src/main/kotlin/com/autentia/tnt/binnacle/usecases/UsersRetrievalUseCase.kt
@@ -1,17 +1,38 @@
 package com.autentia.tnt.binnacle.usecases
 
 import com.autentia.tnt.binnacle.converters.UserResponseConverter
+import com.autentia.tnt.binnacle.entities.User
 import com.autentia.tnt.binnacle.entities.dto.UserResponseDTO
 import com.autentia.tnt.binnacle.repositories.UserRepository
+import com.autentia.tnt.binnacle.repositories.predicates.PredicateBuilder
+import com.autentia.tnt.binnacle.repositories.predicates.UserPredicates
+import io.micronaut.data.jpa.repository.criteria.Specification
 import jakarta.inject.Singleton
+import javax.transaction.Transactional
 
 @Singleton
 class UsersRetrievalUseCase internal constructor(
     private val userRepository: UserRepository,
     private val userResponseConverter: UserResponseConverter
 ) {
-    fun getAllActiveUsers(): List<UserResponseDTO> {
-        return userRepository.find()
-            .map { userResponseConverter.mapUserToUserResponseDTO(it) }
+    @Transactional
+    fun getUsers(userIds: List<Long>?, active: Boolean?): List<UserResponseDTO> {
+       val predicate: Specification<User> = getPredicateFromUsersFilter(userIds, active)
+        val users = userRepository.findAll(predicate)
+        return users.map { userResponseConverter.mapUserToUserResponseDTO(it) }
+    }
+
+    private fun getPredicateFromUsersFilter(userIds: List<Long>?, active: Boolean?): Specification<User>{
+        var predicate: Specification<User> = UserPredicates.ALL
+
+        if(active !== null){
+            predicate = PredicateBuilder.and(predicate, UserPredicates.isActive(active))
+        }
+
+        if(!userIds.isNullOrEmpty()){
+            predicate = PredicateBuilder.and(predicate, UserPredicates.fromUserIds(userIds))
+        }
+
+        return predicate
     }
 }
--- tntconcept-binnacle/src/test/kotlin/com/autentia/tnt/binnacle/usecases/UsersRetrievalUseCaseTest.kt
+++ tntconcept-binnacle/src/test/kotlin/com/autentia/tnt/binnacle/usecases/UsersRetrievalUseCaseTest.kt
@@ -4,6 +4,8 @@ import com.autentia.tnt.binnacle.config.createUser
 import com.autentia.tnt.binnacle.converters.UserResponseConverter
 import com.autentia.tnt.binnacle.entities.dto.UserResponseDTO
 import com.autentia.tnt.binnacle.repositories.UserRepository
+import com.autentia.tnt.binnacle.repositories.predicates.PredicateBuilder
+import com.autentia.tnt.binnacle.repositories.predicates.UserPredicates
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.mockito.kotlin.mock
@@ -15,10 +17,65 @@ internal class UsersRetrievalUseCaseTest {
     private val usersRetrievalUseCase = UsersRetrievalUseCase(userRepository, UserResponseConverter())
 
     @Test
-    fun `should return the list of users`() {
-        whenever(userRepository.find()).thenReturn(listOf(createUser()))
+    fun `should return all users`() {
+        whenever(userRepository.findAll(UserPredicates.ALL)).thenReturn(listOf(createUser()))
 
-        val actual = usersRetrievalUseCase.getAllActiveUsers()
+        val actual = usersRetrievalUseCase.getUsers(null, null)
+
+        assertEquals(listOf(userResponseDTO), actual)
+    }
+
+    @Test
+    fun `should return active users`() {
+        whenever(userRepository.findAll(UserPredicates.isActive(true))).thenReturn(listOf(createUser()))
+
+        val actual = usersRetrievalUseCase.getUsers(null, true)
+
+        assertEquals(listOf(userResponseDTO), actual)
+    }
+
+    @Test
+    fun `should return not active users`() {
+        whenever(userRepository.findAll(UserPredicates.isActive(false))).thenReturn(listOf(createUser()))
+
+        val actual = usersRetrievalUseCase.getUsers(null, false)
+
+        assertEquals(listOf(userResponseDTO), actual)
+    }
+
+    @Test
+    fun `should return list of given users`() {
+        whenever(userRepository.findAll(UserPredicates.fromUserIds(listOf(1,2,3)))).thenReturn(listOf(createUser()))
+
+        val actual = usersRetrievalUseCase.getUsers(listOf(1,2,3), null)
+
+        assertEquals(listOf(userResponseDTO), actual)
+    }
+
+    @Test
+    fun `should return list of active users given a list of users`() {
+        val compositedSpecification =
+            PredicateBuilder.and(
+                UserPredicates.fromUserIds(listOf(1,2,3)) ,
+                UserPredicates.isActive(true)
+            )
+        whenever(userRepository.findAll(compositedSpecification)).thenReturn(listOf(createUser()))
+
+        val actual = usersRetrievalUseCase.getUsers(listOf(1,2,3), true)
+
+        assertEquals(listOf(userResponseDTO), actual)
+    }
+
+    @Test
+    fun `should return list of not active users given a list of users`() {
+        val compositedSpecification =
+            PredicateBuilder.and(
+                UserPredicates.fromUserIds(listOf(1,2,3)) ,
+                UserPredicates.isActive(false)
+            )
+        whenever(userRepository.findAll(compositedSpecification)).thenReturn(listOf(createUser()))
+
+        val actual = usersRetrievalUseCase.getUsers(listOf(1,2,3), false)
 
         assertEquals(listOf(userResponseDTO), actual)
     }
