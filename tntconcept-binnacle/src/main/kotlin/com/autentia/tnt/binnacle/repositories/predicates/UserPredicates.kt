dd057225358ebcd91437c2a5ba927747bf381201
Gregorio Bujor Santos <57153783+GoyoBjS@users.noreply.github.com>
2023-09-18 13:30:30 +0200

    feat: show inactive users in activity pending of approval (#132)
    * feat: add ids and active params in get in UserController
    
    * chore: add test to UserControllerIT
    
    * feat: create UserPredicates file
    
    * feat: change getAllActiveUsers to getUsers & add predicates into UsersRetrievalUseCase
    
    * feat: add findAll users using predicates
    
    * refactor: remove unused test variable
    
    * empty commit
    
    * chore: format code

---
+++ tntconcept-binnacle/src/main/kotlin/com/autentia/tnt/binnacle/repositories/predicates/UserPredicates.kt
@@ -0,0 +1,89 @@
+package com.autentia.tnt.binnacle.repositories.predicates
+
+import com.autentia.tnt.binnacle.entities.User
+import io.micronaut.data.jpa.repository.criteria.Specification
+import javax.persistence.criteria.CriteriaBuilder
+import javax.persistence.criteria.CriteriaQuery
+import javax.persistence.criteria.Predicate
+import javax.persistence.criteria.Root
+
+internal object UserPredicates {
+
+    internal val ALL = EmptySpecification<User>()
+
+    internal fun isActive(active: Boolean) = UserActiveSpecification(active)
+
+    internal fun fromUserIds(ids: List<Long>) = UserFromUserIdsSpecification(ids)
+
+    internal fun userId(id: Long) = UserIdSpecification(id)
+}
+
+class UserIdSpecification(private val id: Long) : Specification<User> {
+
+    override fun toPredicate(
+        root: Root<User>,
+        query: CriteriaQuery<*>,
+        criteriaBuilder: CriteriaBuilder
+    ): Predicate? {
+        return criteriaBuilder.equal(root.get<User>("id"), id)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as UserIdSpecification
+
+        return id == other.id
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
+
+}
+
+class UserFromUserIdsSpecification(private val userIds: List<Long>) : Specification<User> {
+    override fun toPredicate(
+        root: Root<User>,
+        query: CriteriaQuery<*>,
+        criteriaBuilder: CriteriaBuilder
+    ): Predicate? {
+        return root.get<User>("id").`in`(userIds)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return javaClass.hashCode()
+    }
+}
+
+class UserActiveSpecification(private val active: Boolean) : Specification<User> {
+    override fun toPredicate(
+        root: Root<User>,
+        query: CriteriaQuery<*>,
+        criteriaBuilder: CriteriaBuilder
+    ): Predicate? {
+        return criteriaBuilder.equal(root.get<User>("active"), active)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as UserActiveSpecification
+
+        return active == other.active
+    }
+
+    override fun hashCode(): Int {
+        return active.hashCode()
+    }
+
+
+}
