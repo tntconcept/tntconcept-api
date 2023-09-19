565171761f0bd17ba23be2deccf54aa919d9b642
Gregorio Bujor Santos <goyobujor@gmail.com>
2023-09-19 11:00:37 +0200

    feat: add filterByName in UserPredicates and make the case for the UsersRepositoryUseCase

--- tntconcept-binnacle/src/main/kotlin/com/autentia/tnt/binnacle/usecases/UsersRetrievalUseCase.kt
+++ tntconcept-binnacle/src/main/kotlin/com/autentia/tnt/binnacle/usecases/UsersRetrievalUseCase.kt
@@ -2,11 +2,13 @@ package com.autentia.tnt.binnacle.usecases
 
 import com.autentia.tnt.binnacle.converters.UserResponseConverter
 import com.autentia.tnt.binnacle.entities.User
+import com.autentia.tnt.binnacle.entities.dto.UserFilterDTO
 import com.autentia.tnt.binnacle.entities.dto.UserResponseDTO
 import com.autentia.tnt.binnacle.repositories.UserRepository
 import com.autentia.tnt.binnacle.repositories.predicates.PredicateBuilder
 import com.autentia.tnt.binnacle.repositories.predicates.UserPredicates
 import io.micronaut.data.jpa.repository.criteria.Specification
+import io.micronaut.data.model.Pageable
 import jakarta.inject.Singleton
 import javax.transaction.Transactional
 
@@ -16,21 +18,31 @@ class UsersRetrievalUseCase internal constructor(
     private val userResponseConverter: UserResponseConverter
 ) {
     @Transactional
-    fun getUsers(userIds: List<Long>?, active: Boolean?): List<UserResponseDTO> {
-        val predicate: Specification<User> = getPredicateFromUsersFilter(userIds, active)
-        val users = userRepository.findAll(predicate)
+    fun getUsers(userFilter: UserFilterDTO): List<UserResponseDTO> {
+        val predicate: Specification<User> = getPredicateFromUsersFilter(userFilter)
+        val users = if (userFilter.limit !== null) {
+            val pageable = Pageable.from(0, userFilter.limit)
+            val page = userRepository.findAll(predicate, pageable)
+            page.content
+        } else {
+            userRepository.findAll(predicate)
+        }
         return users.map { userResponseConverter.mapUserToUserResponseDTO(it) }
     }
 
-    private fun getPredicateFromUsersFilter(userIds: List<Long>?, active: Boolean?): Specification<User> {
+    private fun getPredicateFromUsersFilter(userFilter: UserFilterDTO): Specification<User> {
         var predicate: Specification<User> = UserPredicates.ALL
 
-        if (active !== null) {
-            predicate = PredicateBuilder.and(predicate, UserPredicates.isActive(active))
+        if (userFilter.active !== null) {
+            predicate = PredicateBuilder.and(predicate, UserPredicates.isActive(userFilter.active))
         }
 
-        if (!userIds.isNullOrEmpty()) {
-            predicate = PredicateBuilder.and(predicate, UserPredicates.fromUserIds(userIds))
+        if (!userFilter.ids.isNullOrEmpty()) {
+            predicate = PredicateBuilder.and(predicate, UserPredicates.fromUserIds(userFilter.ids))
+        }
+
+        if (userFilter.filter !== null) {
+            predicate = PredicateBuilder.and(predicate, UserPredicates.filterByName(userFilter.filter))
         }
 
         return predicate
