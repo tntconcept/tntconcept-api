3327c68f5ff77a11b40cfdc12e2c88369a924e4f
RubenGonzalezAutentia <ruben.gonzalez@izertis.com>
2024-04-16 13:08:14 +0200

    Now subcontracted user don't make an error in TNT classic

--- tntconcept-ddbb/src/main/resources/db/migration/V017__subcontracted_activity.sql
+++ tntconcept-ddbb/src/main/resources/db/migration/V017__subcontracted_activity.sql
@@ -2,8 +2,12 @@ INSERT INTO archimedes_security_role (name) VALUES ('subcontracted-activity-mana
 
 
 
-INSERT INTO User (login, password, passwordExpireDate, roleId, active, name, nif, academicQualification, phone, mobile, street, city, postalCode, provinceId, married, childrenNumber, drivenLicenseType, vehicleType, licensePlate, startDate, categoryId, socialSecurityNumber, bank, account, travelAvailability, workingInClient, email, genre, salary, salaryExtras, securityCard, healthInsurance, notes, photo, contractObservations, agreementId, dayDuration)
-VALUES ('subcontracted', 'd09f2bcb2d8cd02e27b6d1c1ac412b09', NULL, 3, 1, 'subcontracted', '', '', '', '', '', '', '', 28, 1, 2, '', '', '', curdate(), 1, '', '', '', '', 1, 'subcontracted@autentia.com','',0,0,'','','','','', 1, 480);
+INSERT INTO User (login, password, passwordExpireDate, roleId, active, name, nif, academicQualification, phone, mobile, street, city, postalCode, provinceId,
+                  married, childrenNumber, drivenLicenseType, vehicleType, licensePlate, startDate, categoryId, socialSecurityNumber, bank, account,
+                  travelAvailability, workingInClient, email, genre, salary, salaryExtras, securityCard, healthInsurance, notes, photo, contractObservations,
+                  agreementId, dayDuration)
+VALUES ('subcontracted', 'd09f2bcb2d8cd02e27b6d1c1ac412b09', NULL, 3, 1, 'subcontracted', '', '', '', '', '', '', '', 28,
+        1, 0, '', '', '', curdate(), 1, '', '', '', '', 1, 'subcontracted@autentia.com','MAN',0,0,'','','','','', 1, 480);
 
 
 
