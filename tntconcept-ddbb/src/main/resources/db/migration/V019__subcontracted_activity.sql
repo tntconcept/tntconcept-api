b0bf5fc2607c204ab7198d161a3cacafe188bce3
RubenGonzalezAutentia <ruben.gonzalez@izertis.com>
2024-05-07 13:10:51 +0200

    User subcontarcted without name

--- tntconcept-ddbb/src/main/resources/db/migration/V019__subcontracted_activity.sql
+++ tntconcept-ddbb/src/main/resources/db/migration/V019__subcontracted_activity.sql
@@ -7,7 +7,7 @@ INSERT INTO User (login, password, passwordExpireDate, roleId, active, name, nif
                   travelAvailability, workingInClient, email, genre, salary, salaryExtras, securityCard, healthInsurance, notes, photo, contractObservations,
                   agreementId, dayDuration)
 VALUES ('subcontracted', 'd09f2bcb2d8cd02e27b6d1c1ac412b09', NULL, 3, 1, 'subcontracted', '', '', '', '', '', '', '', 28,
-        1, 0, '', '', '', curdate(), 1, '', '', '', '', 1, 'subcontracted@autentia.com','MAN',0,0,'','','','','', 1, 480);
+        1, 0, '', '', '', curdate(), 1, '', '', '', '', 1, '','MAN',0,0,'','','','','', 1, 480);
 
 
 
