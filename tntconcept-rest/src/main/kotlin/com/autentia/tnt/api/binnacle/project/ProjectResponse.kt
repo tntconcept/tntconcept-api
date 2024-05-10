0319d9b0dbd14afc30fb66309c196650ac0cc6cc
RubenGonzalezAutentia <ruben.gonzalez@izertis.com>
2024-05-10 12:58:01 +0200

    Add project billing type to api

--- tntconcept-rest/src/main/kotlin/com/autentia/tnt/api/binnacle/project/ProjectResponse.kt
+++ tntconcept-rest/src/main/kotlin/com/autentia/tnt/api/binnacle/project/ProjectResponse.kt
@@ -1,5 +1,6 @@
 package com.autentia.tnt.api.binnacle.project
 
+import com.autentia.tnt.binnacle.entities.ProjectBillingType
 import com.autentia.tnt.binnacle.entities.dto.ProjectResponseDTO
 import java.time.LocalDate
 
@@ -7,6 +8,7 @@ data class ProjectResponse(
     val id: Long,
     val name: String,
     val open: Boolean,
+    val projectBillingType: ProjectBillingType,
     val organizationId: Long,
     val startDate: LocalDate,
     val blockDate: LocalDate? = null,
@@ -18,6 +20,7 @@ data class ProjectResponse(
                 projectResponseDTO.id,
                 projectResponseDTO.name,
                 projectResponseDTO.open,
+                projectResponseDTO.projectBillingType,
                 projectResponseDTO.organizationId,
                 projectResponseDTO.startDate,
                 projectResponseDTO.blockDate,
