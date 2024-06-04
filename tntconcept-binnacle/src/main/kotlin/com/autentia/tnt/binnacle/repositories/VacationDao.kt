8c4552540ae2fa0554f4ea5800c4b2557eec9b7b
RubenGonzalezAutentia <ruben.gonzalez@izertis.com>
2024-06-04 12:20:56 +0200

    Vacations ordered by the start date

--- tntconcept-binnacle/src/main/kotlin/com/autentia/tnt/binnacle/repositories/VacationDao.kt
+++ tntconcept-binnacle/src/main/kotlin/com/autentia/tnt/binnacle/repositories/VacationDao.kt
@@ -6,33 +6,32 @@ import io.micronaut.data.annotation.Query
 import io.micronaut.data.annotation.Repository
 import io.micronaut.data.repository.CrudRepository
 import java.time.LocalDate
-import java.util.*
 
 @Repository
 internal interface VacationDao : CrudRepository<Vacation, Long> {
     fun findByIdAndUserId(vacationId: Long, userId: Long): Vacation?
 
-    @Query("SELECT h FROM Vacation h WHERE h.userId= :userId AND (h.startDate >= :startDate AND h.endDate <= :endDate OR h.endDate >= :startDate AND h.startDate <= :endDate)")
+    @Query("SELECT h FROM Vacation h WHERE h.userId= :userId AND (h.startDate >= :startDate AND h.endDate <= :endDate OR h.endDate >= :startDate AND h.startDate <= :endDate)  ORDER BY h.startDate")
     fun find(
         startDate: LocalDate,
         endDate: LocalDate,
         userId: Long
     ): List<Vacation>
 
-    @Query("SELECT h FROM Vacation h WHERE h.userId= :userId AND h.chargeYear BETWEEN :startYear AND :endYear")
+    @Query("SELECT h FROM Vacation h WHERE h.userId= :userId AND h.chargeYear BETWEEN :startYear AND :endYear ORDER BY h.startDate")
     fun findBetweenChargeYears(
         startYear: LocalDate,
         endYear: LocalDate,
         userId: Long
     ): List<Vacation>
 
-    @Query("SELECT h FROM Vacation h WHERE h.userId= :userId AND h.chargeYear = :chargeYear")
+    @Query("SELECT h FROM Vacation h WHERE h.userId= :userId AND h.chargeYear = :chargeYear ORDER BY h.startDate")
     fun findByChargeYear(
         chargeYear: LocalDate,
         userId: Long
     ): List<Vacation>
 
-    @Query("SELECT h FROM Vacation h WHERE h.state IN (:states) AND (h.startDate >= :startDate AND h.endDate <= :endDate OR h.endDate >= :startDate AND h.startDate <= :endDate)")
+    @Query("SELECT h FROM Vacation h WHERE h.state IN (:states) AND (h.startDate >= :startDate AND h.endDate <= :endDate OR h.endDate >= :startDate AND h.startDate <= :endDate)  ORDER BY h.startDate")
     fun findByDatesAndStates(
         startDate: LocalDate,
         endDate: LocalDate,
