47bc76fad513e7fec75914373fa365ad3c913cb2
Francisco Javier Martínez Páez <fjmpaez@autentia.com>
2024-06-25 15:02:33 +0200

    Refactor method

--- tntconcept-binnacle/src/main/kotlin/com/autentia/tnt/binnacle/security/ldap/CustomLdapContextAuthenticationMapper.kt
+++ tntconcept-binnacle/src/main/kotlin/com/autentia/tnt/binnacle/security/ldap/CustomLdapContextAuthenticationMapper.kt
@@ -18,7 +18,8 @@ import javax.transaction.Transactional
 @Singleton
 internal class CustomLdapContextAuthenticationMapper : DefaultContextAuthenticationMapper() {
 
-    private val findIdByPrincipalName = "select id, attributes from archimedes_security_subject where principal_name = ?"
+    private val findIdByPrincipalName =
+        "select id, attributes from archimedes_security_subject where principal_name = ?"
     private val findIsUserActive = "select active from User where id = ?"
 
     private val objectMapper: ObjectMapper = ObjectMapper()
@@ -35,37 +36,15 @@ internal class CustomLdapContextAuthenticationMapper : DefaultContextAuthenticat
         username: String?,
         groups: MutableSet<String>?
     ): AuthenticationResponse {
-        val subjectData:Map<String, Any> =
-            jdbcOperations.prepareStatement(findIdByPrincipalName) {
-                it.setString(1, "$username@${appProperties.security.subjectNameSuffix}")
-                val rs = it.executeQuery()
-                if (rs.next()) {
-                    val jsonAttributes = rs.getString("attributes")
-                    val attributesMap:Map<String, Any> = if (jsonAttributes.isBlank()) emptyMap() else objectMapper.readValue(jsonAttributes, Map::class.java) as Map<String, Any>
-                    mapOf(
-                        "id" to rs.getLong("id").toString(),
-                        "attributes" to HashMap(attributesMap)
-                    )
-                } else {
-                    mapOf()
-                }
-            }
-        if(subjectData.isEmpty()) {
+        val subjectData: Map<String, Any> = getSubjectData(username)
+        if (subjectData.isEmpty()) {
             return AuthenticationFailed(AuthenticationFailureReason.USER_NOT_FOUND)
         } else {
             val attributesMap = subjectData["attributes"] as Map<String, Any>
             val userId = attributesMap["sub"] as String
 
-            val active:Boolean? = jdbcOperations.prepareStatement(findIsUserActive) {
-                it.setInt(1, userId.toInt())
-                val rs = it.executeQuery()
-                if (rs.next()) {
-                    rs.getBoolean("active")
-                } else {
-                    null
-                }
-            }
-            if(active==null) {
+            val active: Boolean? = isActive(userId)
+            if (active == null) {
                 return AuthenticationFailed(AuthenticationFailureReason.USER_NOT_FOUND)
             } else if (!active) {
                 return AuthenticationFailed(AuthenticationFailureReason.USER_DISABLED)
@@ -73,4 +52,34 @@ internal class CustomLdapContextAuthenticationMapper : DefaultContextAuthenticat
             return AuthenticationResponse.success(userId, groups, attributesMap)
         }
     }
+
+    private fun isActive(userId: String): Boolean? = jdbcOperations.prepareStatement(findIsUserActive) {
+        it.setInt(1, userId.toInt())
+        val rs = it.executeQuery()
+        if (rs.next()) {
+            rs.getBoolean("active")
+        } else {
+            null
+        }
+    }
+
+    private fun getSubjectData(username: String?): Map<String, Any> =
+        jdbcOperations.prepareStatement(findIdByPrincipalName) {
+            it.setString(1, "$username@${appProperties.security.subjectNameSuffix}")
+            val rs = it.executeQuery()
+            if (rs.next()) {
+                val jsonAttributes = rs.getString("attributes")
+                val attributesMap: Map<String, Any> =
+                    if (jsonAttributes.isBlank()) emptyMap() else objectMapper.readValue(
+                        jsonAttributes,
+                        Map::class.java
+                    ) as Map<String, Any>
+                mapOf(
+                    "id" to rs.getLong("id").toString(),
+                    "attributes" to HashMap(attributesMap)
+                )
+            } else {
+                mapOf()
+            }
+        }
 }
